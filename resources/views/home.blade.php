f033f7f889fb04da7feb5dc9b035617b055f6766
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-12-21 17:36:45 +0100

    Creating new homepage & printing user name on screen using a variable

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<?php
+$user = 'Patrizia';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel Primi Passi</title>
+  </head>
+  <body>
+    <p>Hello, {{ $user }}!</p>
+  </body>
+</html>
