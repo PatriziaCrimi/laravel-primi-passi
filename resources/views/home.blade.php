d5d5e26d844ff103c1967e6e61d1f44c7a70db7f
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-12-21 18:16:18 +0100

    All bugs with SASS fixed. Variable $user to be moved to web.php (in associative array)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@ $user = 'Patrizia';
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/app.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
     <header>
